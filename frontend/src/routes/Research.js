aa240460539baef7c44146c6abe2f2774684e9ff
CS21B052 <cs21b052@iittp.ac.in>
2023-09-17 23:00:24 +0530

    Research Page: Dynamic Cards

--- frontend/src/routes/Research.js
+++ frontend/src/routes/Research.js
@@ -1,37 +1,59 @@
-import React from 'react'
-import Header from './../Components/Header.js';
-import Project from '../Components/Research-card.js';
+import React from 'react';
+import Header from '../Components/Header'; 
+import Project from '../Components/Research-card';
 import CardGroup from 'react-bootstrap/CardGroup';
+import { Button } from 'react-bootstrap';
+import { useState } from 'react';
+
 
 function Research() {
+
+  function add_click(count) {
+    addCard(count + 1);
+    return count + 1;
+  }
+
+  const cardDetails = [
+    {
+      title: "Project Title",
+      professor: "Professor Name",
+      desc: "Short Description about the project",
+      date: "dd-mm-yyyy"
+    },
+  ];
+
+  const [cardCount, setCardCount] = useState(1);
+
+  const addCard = (cardNum) => {
+    const cards = [];
+    for (let i = 0; i < cardNum; i++) {
+      cards.push(
+        <Project
+          key={i}
+          title={cardDetails[0].title}
+          professor={cardDetails[0].professor}
+          desc={cardDetails[0].desc}
+          date={cardDetails[0].date}
+        />
+      );
+    }
+    return cards;
+  };
+
   return (
     <div>
-      <Header/>
-      <h1>Research</h1>
+      <Header />
+      <h3>Research</h3>
       <div className='container'>
         <CardGroup>
-          <Project title="Project Title" professor="Professor Name" desc="Short Description about the project" date="dd-mm-yyyy"/>
-          <Project title="Project Title" professor="Professor Name" desc="Short Description about the project" date="dd-mm-yyyy"/>
-          <Project title="Project Title" professor="Professor Name" desc="Short Description about the project" date="dd-mm-yyyy"/>
-          <Project title="Project Title" professor="Professor Name" desc="Short Description about the project" date="dd-mm-yyyy"/>
-          <Project title="Project Title" professor="Professor Name" desc="Short Description about the project" date="dd-mm-yyyy"/>
-          <Project title="Project Title" professor="Professor Name" desc="Short Description about the project" date="dd-mm-yyyy"/>
-          <Project title="Project Title" professor="Professor Name" desc="Short Description about the project" date="dd-mm-yyyy"/>
-          <Project title="Project Title" professor="Professor Name" desc="Short Description about the project" date="dd-mm-yyyy"/>
-          <Project title="Project Title" professor="Professor Name" desc="Short Description about the project" date="dd-mm-yyyy"/>
-          <Project title="Project Title" professor="Professor Name" desc="Short Description about the project" date="dd-mm-yyyy"/>
-          <Project title="Project Title" professor="Professor Name" desc="Short Description about the project" date="dd-mm-yyyy"/>
-          <Project title="Project Title" professor="Professor Name" desc="Short Description about the project" date="dd-mm-yyyy"/>
-          <Project title="Project Title" professor="Professor Name" desc="Short Description about the project" date="dd-mm-yyyy"/>
-          <Project title="Project Title" professor="Professor Name" desc="Short Description about the project" date="dd-mm-yyyy"/>
-          <Project title="Project Title" professor="Professor Name" desc="Short Description about the project" date="dd-mm-yyyy"/>
-          <Project title="Project Title" professor="Professor Name" desc="Short Description about the project" date="dd-mm-yyyy"/>
-          <Project title="Project Title" professor="Professor Name" desc="Short Description about the project" date="dd-mm-yyyy"/>
-          <Project title="Project Title" professor="Professor Name" desc="Short Description about the project" date="dd-mm-yyyy"/>
+          {addCard(cardCount)}
         </CardGroup>
       </div>
+      <div>
+        <button className='btn btn-success addProjectButton' onClick={() => setCardCount(add_click(cardCount))}>Add Project</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Research
+export default Research;
