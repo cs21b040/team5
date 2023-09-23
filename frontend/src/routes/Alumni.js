e140e5b2b59ac551c64ec2355f15e5399af35c82
“CS21B029” <cs21b029@iittp.ac.in>
2023-09-23 16:35:05 +0530

    alumni basic ui added

--- frontend/src/routes/Alumni.js
+++ frontend/src/routes/Alumni.js
@@ -1,12 +1,54 @@
-import React from 'react'
-import Header from './../Components/Header.js';
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+import Header from '../Components/Header';
+import alumnipic from '../Components/assets/alumnipic.jpg';
+
 function Alumni() {
+  const [cardCount, setCardCount] = useState(0);
+
+  function add_click(count) {
+    setCardCount(count + 1);
+  }
+
+  const addCard = (cardNum) => {
+    const cards = [];
+    for (let i = 0; i < cardNum; i++) {
+      cards.push(
+        <div className="alumniCard" key={i}>
+          <Card style={{ width: '20rem' }}>
+            <Card.Img variant="top" src={alumnipic}/>
+            <Card.Body>
+              <Card.Title>Alumni Name</Card.Title>
+              <Card.Text>
+                Company Name <br/>
+                Institute Name <br/>
+                Small description about him
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </div>
+      );
+    }
+    return cards;
+  };
+
   return (
     <div>
-      <Header/>
-      <p>this is Alumni</p>
+      <Header />
+      <h3>Alumni</h3>
+      <div className='alumni'>
+        <CardGroup>
+          {addCard(cardCount)}
+        </CardGroup>
+      </div>
+      <div>
+        <button className='btn btn-success addProjectButton' onClick={() => add_click(cardCount)}>addCard</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Alumni
+export default Alumni;
