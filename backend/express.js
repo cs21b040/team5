59c18bbb8e254d4b863f03fbb07ed90b1cb1592e
cs21b040 <cs21b040@iittp.ac.in>
2023-10-03 14:54:13 +0530

    frontEnd imlemented using socket.io

--- backend/express.js
+++ backend/express.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({extended:true}));
 const port= process.env.PORT;
-app.listen(port,()=>{
+const server = app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
 });
 app.get('/',(req,res)=>{
@@ -26,9 +26,6 @@ app.use('/api/chat',chatRoutes);
 app.use('/api/messages',messageRoutes);
 app.use('/api/research', projectRoutes);
 app.use('/api/alumni', alumniRoutes)
-app.get("/api/chat",(req,res)=>{
-    res.send(chats);
-})
 app.get("/api/chat/:id",(req,res)=>{
     // console.log(req.params.id);
     const singleChat=chats.find((c)=>c._id ===req.params.id);
@@ -38,3 +35,42 @@ app.get("/api/chat/:id",(req,res)=>{
 app.get("/subjects",(req,res)=>{
     res.send(subjects);
 });
+
+const io=require('socket.io')(server,{
+    pingTimeout:60000,
+    cors:{
+        origin:"http://localhost:3000",
+    },
+});
+
+io.on('connection',(socket)=>{
+    console.log("Socket connected");
+    socket.on('setup',(userData)=>{
+        socket.join(userData._id);
+        socket.emit('connected');
+    });
+    socket.on('join room',(room)=>{
+        socket.join(room);
+        console.log("Joined room" + room);
+    });
+    socket.on('typing',(room)=>{
+        socket.in(room).emit("typing");
+    }); 
+    socket.on('stop typing',(room)=>{
+        socket.in(room).emit("stop typing");
+    });
+    socket.on('new msg',(newMessageRecieved)=>{
+        var chat = newMessageRecieved.chat;
+        if(!chat.users){
+            return console.log("users not defined");
+        }
+        chat.users.forEach(user =>{
+            if(user._id == newMessageRecieved.sender._id) return;
+            socket.in(user._id).emit("message recieved",newMessageRecieved);
+        })
+        // socket.off("user left",()=>{
+        //     console.log("user left");
+        //     socket.leave(chat._id);
+        // });
+    })
+})
