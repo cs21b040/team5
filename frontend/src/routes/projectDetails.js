8ae748409a08e52a4c02bbb6d45e627a8380c632
CS21B052 <cs21b052@iittp.ac.in>
2023-11-05 09:39:36 +0530

    Download Files in research page

--- frontend/src/routes/projectDetails.js
+++ frontend/src/routes/projectDetails.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import './../Components/Styles/research.css';
+import { ChatState } from '../context/chatProvider';
+import axios from 'axios';
 import { useLocation } from 'react-router-dom';
 import '../Components/Styles/projectdetails.css';
 import Button from 'react-bootstrap/Button';
 
 function ProjectDetails() {
+  const { user } = ChatState();
   const location = useLocation();
   const project = location.state.project;
+
+  const downloadFile = async () => {
+    try {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+      const { data } = await axios.get(
+        `http://localhost:5000/api/research/download/${project._id}`,
+        config
+      ); 
+      const fileURL = "/" + data.status;
+      let alink = document.createElement("a");
+      alink.href = fileURL;
+      alink.download = data.status;
+      alink.click();
+    }
+    catch (error) {
+      console.error(error);
+    }
+  }
 
   return (
       <div className='projectdetails'>
@@ -39,6 +64,10 @@ function ProjectDetails() {
           <div className='projectdetails'>
             <h4>Abstract:</h4>          
             <p style={{whiteSpace:'break-spaces'}}>{project.abstract}</p>
+          </div>
+          <div className='projectdetails'>
+            <h4>Download File:</h4>
+            <Button variant='primary' onClick={downloadFile}>Download</Button>
           </div>
         </div>
         <Button variant='primary' href='/research'>Back</Button>
