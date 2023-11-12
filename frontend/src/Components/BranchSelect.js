98904708081ba0854d2085a5f92c094ee80d0b70
CS21B052 <cs21b052@iittp.ac.in>
2023-11-12 14:48:19 +0530

    Styling

--- frontend/src/Components/BranchSelect.js
+++ frontend/src/Components/BranchSelect.js
@@ -1,17 +1,18 @@
 
 import React from 'react'
 import Offcanvas from 'react-bootstrap/Offcanvas';
-import { useState,useEffect  } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Form from 'react-bootstrap/Form';
 import axios from 'axios';
 import { ChatState } from '../context/chatProvider';
 import { ToastContainer, toast } from 'react-toastify';
+import { BiBookBookmark } from 'react-icons/bi';
 
 function BranchSelect() {
   const {
     user,
-} = ChatState();
+  } = ChatState();
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
@@ -22,7 +23,7 @@ function BranchSelect() {
 
   async function getBranches() {
     try {
-      const response = await axios.get('http://localhost:5000/api/academics/',{
+      const response = await axios.get('http://localhost:5000/api/academics/', {
         headers: {
           Authorization: `Bearer ${user.token}`,
         }
@@ -38,31 +39,31 @@ function BranchSelect() {
   }
 
   useEffect(() => {
-    if(!user) return;
+    if (!user) return;
     getBranches();
-  },[user, branches]); 
-  function fun(e){
+  }, [user, branches]);
+  function fun(e) {
     setBranch(e.target.innerHTML);
     handleClose();
-    const x=e.target.innerHTML;
+    const x = e.target.innerHTML;
     navigate(`/Academic/${x}`);
   }
-  const reqBranch=async ()=>{
-    const br=document.getElementById('br').value.toString();
-    const config={
-      headers:{
+  const reqBranch = async () => {
+    const br = document.getElementById('br').value.toString();
+    const config = {
+      headers: {
         Authorization: `Bearer ${user.token}`,
       }
     };
     try {
-      const data=await axios.post('http://localhost:5000/api/academicAdmin/',{
-        user:user,
-        Branch:br
-      },config);
-      if(!data) toast.error("Request not sent");
+      const data = await axios.post('http://localhost:5000/api/academicAdmin/', {
+        user: user,
+        Branch: br
+      }, config);
+      if (!data) toast.error("Request not sent");
       else toast.success("Request sent");
     } catch (error) {
-      
+
     }
   }
   return (
@@ -77,32 +78,43 @@ function BranchSelect() {
         draggable
         theme="light"
       />
-      <button variant="primary"  onClick={handleShow} style={{backgroundColor: 'transparent', margin: '0', border: '0',color:'#000000a6'}}>Academics</button>
-      <Offcanvas show={show} onHide={handleClose}>
-        <Offcanvas.Header closeButton>
-          <Offcanvas.Title>ACADEMICS</Offcanvas.Title>
+      <button variant="primary" onClick={handleShow} style={{ backgroundColor: 'transparent', margin: '0', border: '0', color: '#000000a6' }}>Academics</button>
+      <Offcanvas show={show} onHide={handleClose} style={{ backgroundColor: '#f8f9fa' }}>
+        <Offcanvas.Header closeButton style={{ borderBottom: '1px solid #dee2e6' }}>
+          <Offcanvas.Title style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <span style={{ fontSize: '2rem', fontWeight: 'bold' }}>Academics</span>
+            <BiBookBookmark style={{ marginLeft: '15px' }} size={30} />
+          </Offcanvas.Title>
         </Offcanvas.Header>
-        <Offcanvas.Body>
-            <ul style={{listStyle:'none'}}>
+        <Offcanvas.Body style={{ padding: '5px', color: '#343a40' }}>
+          <ul style={{ listStyle: 'none' }}>
             {branches.map((br, index) => (
-              <li key={index} style={{ padding: '10px', border:'0'}}>
-                <h6 style={{ textDecoration: 'none', color: '#3B4045', cursor: 'pointer' }} onClick={fun}>{br.name}</h6>
+              <li key={index} style={{ padding: '10px', border: '0', transition: 'all 0.3s ease', cursor: 'pointer' }}
+                onMouseEnter={(e) => {
+                  e.currentTarget.style.transform = 'translateY(-3px)';
+                  e.currentTarget.style.backgroundColor = '#d3d3d3';
+                }}
+                onMouseLeave={(e) => {
+                  e.currentTarget.style.transform = '';
+                  e.currentTarget.style.backgroundColor = '';
+                }}>
+                <h6 style={{ textDecoration: 'none', color: '#3B4045' }}>{br.name}</h6>
               </li>
             ))}
-            </ul>
+          </ul>
         </Offcanvas.Body>
         <div>
           <Form>
             <Form.Group className="mb-3" controlId="br">
-                <Form.Control placeholder="Enter branch" />
-                <button onClick={()=>{
-                  reqBranch();
-                }}>Request</button>
+              <Form.Control placeholder="Enter branch" />
+              <button onClick={() => {
+                reqBranch();
+              }}>Request</button>
             </Form.Group>
           </Form>
         </div>
       </Offcanvas>
-      
+
     </div>
   )
 }
